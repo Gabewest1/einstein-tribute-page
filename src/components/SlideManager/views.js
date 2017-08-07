460ad086bb84ec604b3081c2d7366020e2932a36
Gabriel West <gabe_west1@yahoo.com>
2017-08-06 20:10:22 -0500

    Created SlideComponent

---
+++ src/components/SlideManager/views.js
@@ -0,0 +1,28 @@
+import React from "react"
+import sizeMe from "react-sizeme"
+
+import {
+    Slide,
+    CurrentSlide,
+    Header,
+    Content,
+    SlideButton,
+    ParallaxImage
+} from "./styles"
+
+export class SlideComponent extends React.Component {
+    componentDidMount() {
+        let = { name, nextOrPrev, setComponentsPosition } = this.props
+        let top = window.getComputedStyle(this.component).getPropertyValue("top")
+        let left = window.getComputedStyle(this.component).getPropertyValue("left")
+
+        this.props.setComponentsPosition(name, nextOrPrev, {top, left})
+    }
+    render() {
+        let { Component } = this.props
+
+        return (
+            <Component { ...this.props } />
+        )
+    }
+}
