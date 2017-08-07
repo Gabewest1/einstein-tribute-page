831894311805e575c8a354bdd1ccfc127ff56d07
Gabriel West <gabe_west1@yahoo.com>
2017-08-07 18:51:44 -0500

    Created seperate file to place the array of slides

---
+++ src/redux/Slides/slidesInitialState.js
@@ -0,0 +1,202 @@
+export default [
+    {
+        gridStyle: `"header header header header header header header header"
+                       "header header header header header header header header"
+                       ". . . . . . . ."
+                       "content content content content content content content content"
+                       "content content content content content content content content"
+                       "content content content content content content content content"
+                       ". . . . . . . ."
+                       "button button button . . . . ."`,
+        header: {
+            content: "This is the first Header for ya boy Einstein",
+            start: {
+                top: "100%",
+                left: 0
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        },
+        body: {
+            content: "This is the first body paragraph for ya boy Einstein. Dude was a real G, if you know what i mean? This is the first body paragraph for ya boy Einstein. Dude was a real G, if you know what i mean? This is the first body paragraph for ya boy Einstein. Dude was a real G, if you know what i mean? This is the first body paragraph for ya boy Einstein. Dude was a real G, if you know what i mean?",
+            start: {
+                top: "100%",
+                left: 0
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        },
+        button: {
+            content: "Next",
+            start: {
+                top: "100%",
+                left: 0
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        },
+        image1: {
+            src: "/assets/images/young-left.png",
+            start: {
+                top: "100%",
+                left: 0
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        },
+        image2: {
+            src: "/assets/images/young-right.png",
+            start: {
+                top: "100%",
+                left: 0
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        }
+    },
+    {
+        gridStyle: `"content content content content content content content content"
+                       "content content content content content content content content"
+                       "content content content content content content content content"
+                       ". . . . . . . ."
+                       ". . . . . . . ."
+                       "header header header header header header header header"
+                       "header header header header header header header header"
+                       "button button button . . . . ."`,
+        header: {
+            content: "A new header has appeared... and DON'T forget E=mc2",
+            start: {
+                top: "0",
+                left: "100%"
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        },
+        body: {
+            content: "Einstein developed the theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics).[1][6]:274 Einstein's work is also known for its influence on the philosophy of science.[7][8] Einstein is best known by the general public for his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation').[9] He received the 1921 Nobel Prize in Physics "for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect",[10] a pivotal step in the evolution of quantum theory.",
+            start: {
+                top: "0",
+                left: "100%"
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        },
+        button: {
+            content: "Next",
+            start: {
+                top: "100%",
+                left: 0
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        },
+        image1: {
+            src: "/assets/images/young-left.png",
+            start: {
+                top: "100%",
+                left: 0
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        },
+        image2: {
+            src: "/assets/images/young-right.png",
+            start: {
+                top: "100%",
+                left: 0
+            },
+            normal: {
+                top: 0,
+                left: 0
+            },
+            end: {
+                top: "-100",
+                left: 0
+            }
+        }
+    },
+    {
+        gridStyle: `"content content content content content content content content"
+                       "content content content content content content content content"
+                       "content content content content content content content content"
+                       ". . . . . . . ."
+                       ". . . . . . . ."
+                       "header header header header header header header header"
+                       "header header header header header header header header"
+                       "button button button . . . . ."`,
+        header1Top: "-100%",
+        header1Left: 0,
+        header2Top: 0,
+        header2Left: 0,
+        content1Top: "-100%",
+        content1Left: 0,
+        content2Top: 0,
+        content2Left: 0,
+        image1Top: "-100%",
+        image1Left: 0,
+        image1Src: "/assets/images/young-left.png",
+        image2Top: 0,
+        image2Left: 0,
+        image2Src: "/assets/images/young-right.png",
+        image3Top: 0,
+        image3Left: 0,
+        image3Src: "/assets/images/old-left.png",
+        image4Top: "100%",
+        image4Left: "100%",
+        image4Src: "/assets/images/young-right.png"
+    }
+]
