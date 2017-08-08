da5ef90fbc1d8b7bfe562e854c10181ea2a8c9be
Gabriel West <gabe_west1@yahoo.com>
2017-08-07 20:23:20 -0500

    Removed the slides intial state from the reducer and into its own file.

--- src/redux/Slides/reducers.js
+++ src/redux/Slides/reducers.js
@@ -1,5 +1,6 @@
 import { createActions, handleActions } from "redux-actions"
 import { combineReducers } from "redux"
+import slidesInitialState from "./slidesInitialState"
 
 const {
     gotoNextSlide,
@@ -35,98 +36,7 @@ const slideTrackerReducer = handleActions({
     [nextSlideFinished]: (state, action) => ({...state, currentSlideIndex: state.currentSlideIndex + 1}),
     [previousSlideFinished]: (state, action) => ({...state, currentSlideIndex: state.currentSlideIndex - 1})
 }, {
-    slides: [
-        {
-            gridStyle: `"header header header header header header header header"
-                       "header header header header header header header header"
-                       ". . . . . . . ."
-                       "content content content content content content content content"
-                       "content content content content content content content content"
-                       "content content content content content content content content"
-                       ". . . . . . . ."
-                       "button button button . . . . ."`,
-            header1Top: 0,
-            header1Left: 0,
-            header2Top: "100%",
-            header2Left: "100%",
-            content1Top: 0,
-            content1Left: 0,
-            content2Top: "100%",
-            content2Left: "100%",
-            image1Top: 0,
-            image1Left: 0,
-            image1Src: "/assets/images/young-left.png",
-            image2Top: 0,
-            image2Left: 0,
-            image2Src: "/assets/images/young-right.png",
-            image3Top: "100%",
-            image3Left: 0,
-            image3Src: "/assets/images/old-left.png",
-            image4Top: "100%",
-            image4Left: "100%",
-            image4Src: "/assets/images/young-right.png"
-        },
-        {
-            gridStyle: `"content content content content content content content content"
-                       "content content content content content content content content"
-                       "content content content content content content content content"
-                       ". . . . . . . ."
-                       ". . . . . . . ."
-                       "header header header header header header header header"
-                       "header header header header header header header header"
-                       "button button button . . . . ."`,
-            header1Top: "-100%",
-            header1Left: 0,
-            header2Top: 0,
-            header2Left: 0,
-            content1Top: "-100%",
-            content1Left: 0,
-            content2Top: 0,
-            content2Left: 0,
-            image1Top: "-100%",
-            image1Left: 0,
-            image1Src: "/assets/images/young-left.png",
-            image2Top: 0,
-            image2Left: 0,
-            image2Src: "/assets/images/young-right.png",
-            image3Top: 0,
-            image3Left: 0,
-            image3Src: "/assets/images/old-left.png",
-            image4Top: "100%",
-            image4Left: "100%",
-            image4Src: "/assets/images/young-right.png"
-        },
-        {
-            gridStyle: `"content content content content content content content content"
-                       "content content content content content content content content"
-                       "content content content content content content content content"
-                       ". . . . . . . ."
-                       ". . . . . . . ."
-                       "header header header header header header header header"
-                       "header header header header header header header header"
-                       "button button button . . . . ."`,
-            header1Top: "-100%",
-            header1Left: 0,
-            header2Top: 0,
-            header2Left: 0,
-            content1Top: "-100%",
-            content1Left: 0,
-            content2Top: 0,
-            content2Left: 0,
-            image1Top: "-100%",
-            image1Left: 0,
-            image1Src: "/assets/images/young-left.png",
-            image2Top: 0,
-            image2Left: 0,
-            image2Src: "/assets/images/young-right.png",
-            image3Top: 0,
-            image3Left: 0,
-            image3Src: "/assets/images/old-left.png",
-            image4Top: "100%",
-            image4Left: "100%",
-            image4Src: "/assets/images/young-right.png"
-        }
-    ],
+    slides: slidesInitialState,
     currentSlideIndex: 0
 })
 
