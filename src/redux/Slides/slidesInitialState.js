c9ba5dffbca6f432d3f505c6d835fcc051686334
Gabewest1 <gabe_west1@yahoo.com>
2017-08-16 17:55:48 -0500

    Made all the top and left values be in %.

--- src/redux/Slides/slidesInitialState.js
+++ src/redux/Slides/slidesInitialState.js
@@ -11,67 +11,67 @@ export default [
                        "button button button . . . . ."`,
         header: {
             content: "This is the first Header for ya boy Einstein",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: 0
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         body: {
             content: "This is the first body paragraph for ya boy Einstein. Dude was a real G, if you know what i mean? This is the first body paragraph for ya boy Einstein. Dude was a real G, if you know what i mean? This is the first body paragraph for ya boy Einstein. Dude was a real G, if you know what i mean? This is the first body paragraph for ya boy Einstein. Dude was a real G, if you know what i mean?",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: 0
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         button: {
             content: "Next1",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: 0
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         image1: {
             src: "/assets/images/young-left.png",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: 0
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         image2: {
             src: "/assets/images/young-right.png",
-            top: "0",
-            left: "0",
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: "0"
+                left: "0%"
             },
             end: {
-                top: "0",
-                left: "0"
+                top: "0%",
+                left: "0%"
             }
         }
     },
@@ -87,73 +87,73 @@ export default [
                        "button button button . . . . ."`,
         header: {
             content: "A new header has appeared... and DON'T forget E=mc2",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
-                top: "0",
+                top: "0%",
                 left: "100%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         body: {
             content: "Einstein developed the theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics).[1][6]:274 Einstein's work is also known for its influence on the philosophy of science.[7][8] Einstein is best known by the general public for his mass–energy equivalence formula E = mc2 (which has been dubbed 'the world's most famous equation').[9] He received the 1921 Nobel Prize in Physics 'for his services to theoretical physics, and especially for his discovery of the law of the photoelectric effect',[10] a pivotal step in the evolution of quantum theory.",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
-                top: "0",
+                top: "0%",
                 left: "100%"
             },
             end: {
-                top: "-100",
-                left: 0
+                top: "-100%",
+                left: "0%"
             }
         },
         button: {
             content: "Next2",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: 0
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         image1: {
             src: "/assets/images/old-left.png",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: 0
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         image2: {
             src: "/assets/images/young-right.png",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
-                top: "0",
-                left: 0
+                top: "0%",
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         }
     },
     {
         id: 2,
-        gridStyle: `"content content content content content content content content"
+        gridStyle: `". . . . . . . ."
                        ". . . . . . . ."
                        "header header header header header header header header"
                        "header header header header header header header header"
@@ -163,8 +163,8 @@ export default [
                        "button button button . . . . ."`,
         header: {
             content: "The Dawn of a New Era",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
                 left: "100%"
@@ -176,54 +176,54 @@ export default [
         },
         body: {
             content: "Einstein and Marić married in January 1903. In May 1904, their first son, Hans Albert Einstein, was born in Bern, Switzerland. Their second son, Eduard, was born in Zürich in July 1910. In April 1914 they moved to Berlin. After a few months his wife returned to Zürich with their sons, after learning that Einstein's chief romantic attraction was his first and second cousin Elsa.",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
-                top: "0",
+                top: "0%",
                 left: "100%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         button: {
             content: "Next3",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: 0
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         image1: {
             src: "/assets/images/old-left.png",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
                 top: "100%",
-                left: 0
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         },
         image2: {
             src: "/assets/images/young-right.png",
-            top: 0,
-            left: 0,
+            top: "0%",
+            left: "0%",
             start: {
-                top: "0",
-                left: 0
+                top: "0%",
+                left: "0%"
             },
             end: {
                 top: "-100%",
-                left: 0
+                left: "0%"
             }
         }
     }
